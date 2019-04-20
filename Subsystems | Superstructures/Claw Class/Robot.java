7620326b824698c0605ab71048c64ca2a5fbaf31
Team Cold Logic <coldlogic3617@gmail.com>
2019-04-20 10:39:40 -0400

    Update Robot.java

--- Subsystems | Superstructures/Claw Class/Robot.java
+++ Subsystems | Superstructures/Claw Class/Robot.java
@@ -14,20 +14,19 @@ private final SpeedController LFingerMotor = new SpeedController(3);
 
 
 //Declare the Claw object, arguments are the two "hand" SpeedControllers
-private final Claw ballClaw = new Claw(RHandMotor,LHandMotor);
+private final Claw simpleClaw = new Claw(RHandMotor,LHandMotor);
 
 //If your claw is articulating, then declar an ArticulateClaw class with four SpeedControllers as the arguments
-private final Claw crateClaw = new Claw(RHandMotor,LHandMotor,RFingerMotor,LFingerMotor);
+private final Claw articulateClaw = new Claw(RHandMotor,LHandMotor,RFingerMotor,LFingerMotor);
 
 
 //Declare your Drive Station joystick or controller
 private final Joystick clawStick = new Joystick(1);
 
 //Declare your Joystick buttons, arguments are the joystick and the button number
-private final JoystickButton openClawButton = new JoystickButton(clawStick,1);
-private final JoystickButton closeClawButton = new JoystickButton(clawStick,2);
+private final JoystickButton clawButton = new JoystickButton(clawStick,1);
 
-private final JoystickButton articulateClawButton = new JoystickButton(clawStick,3);
+private final JoystickButton pinchButton = new JoystickButton(clawStick,2);
 
 public class Robot extends IterativeRobot {
 
@@ -54,16 +53,13 @@ public class Robot extends IterativeRobot {
   @Override
   public void teleopPeriodic() {
     
-    //Open / Close Claw set to buttons
+    //When the button is pressed the claw will either open or close depending on is current state
     
-    if (openClawButton.get()) {
-      claw.open();
-    }
-    else if (closeClawButton.get()) {
-      claw.close();
+    if (clawButton.get()) {
+      simpleClaw.openClaw();
     }
     else {
-      claw.stop();
+      simpleClaw.stopClaw();
     }
     
   }
@@ -71,5 +67,20 @@ public class Robot extends IterativeRobot {
 
   @Override
   public void testPeriodic() {
+    
+    if (clawButton.get()) {
+      articulateClaw.openClaw();
+    }
+    else {
+      articulateClaw.stopClaw();
+    }
+    
+    if (pinchButton.get()) {
+      artuclateClaw.openPinch();
+    }
+    else {
+      articulateClaw.stopPunch();
+    }
+    
   }
 }
